374315d7793832ee62270a78e7522ee709af18c3
Eric Mauldin <emauldin84@gmail.com>
2019-06-24 13:43:34 -0400

    simple click counter with minimal styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    state = {
+      count: 0,
+    }
+
+
+  handleIncrement = () => {
+    let counter = this.state.count
+    this.setState({
+      count: counter += 1
+    })
+  }
+  handleDecrement = () => {
+    let counter = this.state.count
+    this.setState({
+      count: counter -= 1 
+    })
+  }
+  handleReset = () => {
+    this.setState({
+      count: 0
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container" >
+          <p>{this.state.count}</p>
+          <div className="controls">
+            <button onClick={this.handleDecrement} className={'decrement'}>-</button>
+            <button onClick={this.handleReset} className={'reset'}>reset</button>
+            <button onClick={this.handleIncrement} className={'increment'}>+</button>
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+
 }
 
 export default App;
